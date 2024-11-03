1ecfff5030c524886646ba993758aaa12af7becb
Tushar Singh <93639266+Tushar-2k21@users.noreply.github.com>
2024-11-03 20:15:58 +0530

    Update notification.routes.js

--- backend/src/routes/notification.routes.js
+++ backend/src/routes/notification.routes.js
@@ -1,14 +1,14 @@
 
 import { getNotifications, markAsRead } 
-from "../controllers/notification.controller.js";
-import { verifyJWT } from "../middlewares/auth.middleware.js";
-import {Router} from "express";
+from '../controllers/notification.controller.js';
+import { verifyJWT } from '../middlewares/auth.middleware.js';
+import {Router} from 'express';
 
 const router = Router();
 
 router.use(verifyJWT)
 
-router.route("/").get(getNotifications)
-router.route("/mark-read").put(markAsRead)
+router.route('/').get(getNotifications)
+router.route('/mark-read').put(markAsRead)
 
 export default router;
