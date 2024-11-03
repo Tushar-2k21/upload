76562ad2f75203f393968b1b43fea376261bbc02
Tushar Singh <93639266+Tushar-2k21@users.noreply.github.com>
2024-11-03 20:16:34 +0530

    Update subscription.routes.js

--- backend/src/routes/subscription.routes.js
+++ backend/src/routes/subscription.routes.js
@@ -1,12 +1,11 @@
 import { Router } from "express";
-import { toggleSubscription, getUserChannelSubscribers, getSubscribedChannels } from "../controllers/subscription.controller.js"
+import { toggleSubscription, getUserChannelSubscribers, getSubscribedChannels } from '../controllers/subscription.controller.js'
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
-// router.post(, verifyJWT, toggleSubscription);
+router.post('/toggle/:channelId', verifyJWT, toggleSubscription);
+router.get('/subscribers/:channelId', verifyJWT, getUserChannelSubscribers);
+router.get('/subscribed', verifyJWT, getSubscribedChannels);
 
-router.route("/toggle/:channelId").post(verifyJWT, toggleSubscription)
-router.route("/subscribers/:channelId").get(verifyJWT, getUserChannelSubscribers)
-router.route("/subscribed").get(verifyJWT, getSubscribedChannels)
 
 export default router;
