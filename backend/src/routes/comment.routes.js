c027f314b54513244c94bdcb4fcc1eaf07c5a53e
Tushar Singh <93639266+Tushar-2k21@users.noreply.github.com>
2024-11-03 20:14:40 +0530

    Update comment.routes.js

--- backend/src/routes/comment.routes.js
+++ backend/src/routes/comment.routes.js
@@ -3,20 +3,20 @@ import {
     addComment,
     updateComment,
     deleteComment
-} from "../controllers/comment.controller.js";
+} from '../controllers/comment.controller.js';
 
-import { Router } from "express";
-import {verifyJWT} from "../middlewares/auth.middleware.js";
+import { Router } from 'express';
+import {verifyJWT} from '../middlewares/auth.middleware.js';
 const router = Router();
 
 router.use(verifyJWT);
 
-router.route("/:videoId").get(getVideoComments)
+router.route('/:videoId').get(getVideoComments)
 
-router.route("/:videoId").post( addComment)
+router.route('/:videoId').post( addComment)
     
 
-router.route("/:commentId")
+router.route('/:commentId')
     .delete(deleteComment)
     .put(updateComment);
 
